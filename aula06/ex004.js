ed86001640fb7611e3695dd9dd61a1e9573e2a2c
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-29 10:48:05 +0200

    Update ex004.js

--- aula06/ex004.js
+++ aula06/ex004.js
@@ -2,4 +2,7 @@ var nome = window.prompt('Digite o seu Nome: ')
 document.write(`Ola <strong>${nome}</strong> seu nome tem ${nome.length} letras `)
 // para quebrar linhas podemos usar o <br> dentro di js ou writeln
 document.writeln(`Seu nome em maiusculas ${nome.toLocaleUpperCase()}`)
+var n = 433434.3434
+//n.toLocaleString('pt-BR',{style: 'currency', currency: 'MZN'})- no node mostra 
+//document.write(`${n}`)
 //alert(`Ola ${nome}`)
