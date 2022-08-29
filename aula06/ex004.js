f5459d2c5da92d13441a5087c22a871cafa9bd98
ericiantonio <108551667+eric-antonio@users.noreply.github.com>
2022-08-29 10:06:38 +0200

    Formatação de strings

--- aula06/ex004.js
+++ aula06/ex004.js
@@ -1,3 +1,5 @@
 var nome = window.prompt('Digite o seu Nome: ')
-document.write(`Seu nome tem ${nome.length} letras`)
-alert(`Ola ${nome}`)
+document.write(`Ola <strong>${nome}</strong> seu nome tem ${nome.length} letras `)
+// para quebrar linhas podemos usar o <br> dentro di js ou writeln
+document.writeln(`Seu nome em maiusculas ${nome.toLocaleUpperCase()}`)
+//alert(`Ola ${nome}`)
