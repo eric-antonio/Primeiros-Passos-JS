314c4cddc3d79a95931a864fe9afbd88a0727dc4
ericiantonio <ericantoniodev@gmail.com>
2022-09-01 22:55:25 +0200

    Update ex011.js

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,7 +1,8 @@
 var idade = 22
+console.log(`Voce tem ${idade} anos!`)
 if(idade < 18){
     console.log(`Menor de Idade!`)
-}else if(idade <= 18 ){
+}else if(idade <= 18 || idade >= 65){
     console.log(`Voto Opcional!`)
 } else {
     console.log(`FPD Vai votar!`)
