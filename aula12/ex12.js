4193f9f48d854df12476ebc03e98d9119a6ac1ff
ericiantonio <ericantoniodev@gmail.com>
2022-09-01 23:06:18 +0200

    Update ex12.js

--- aula12/ex12.js
+++ aula12/ex12.js
@@ -2,9 +2,9 @@ var hora = 20
 console.log(`Agora são extamente ${hora} horas.`)
 if(hora < 6 ){
     console.log(`São :${hora} da Madrugada!`)
-}else if(hora >= 6){
+}else if(hora >= 6 && hora < 12){
     console.log(`São : ${hora} da Manhã!`)
-}else if(hora >= 12){
+}else if(hora >= 12 && hora <18){
     console.log(`São: ${hora} da Tarde!`)
 }else if(hora >= 18){
     console.log(`São : ${hora} da Noite!`)
