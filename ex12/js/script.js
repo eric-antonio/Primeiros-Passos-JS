10219ea2ea71032c49dbcea3511849b58942bc07
ericiantonio <ericantoniodev@gmail.com>
2022-09-02 08:59:53 +0200

    Data e minutos

--- ex12/js/script.js
+++ ex12/js/script.js
@@ -1,1 +1,14 @@
-alert('Ola, Mundo!')
+function caregar(){
+    var msg = window.document.getElementById('msg')
+    var imagem = window.document.getElementById('imagem')
+    var data =  new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 0 && hora < 12){
+        msg.innerHTML = `Bom Dia! <br> Agora são ${hora}h:${minutos}m.`
+    }
+
+}
