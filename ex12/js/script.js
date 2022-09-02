7adfb74d56b903b345e1666b038bb8553b16ecd6
ericiantonio <ericantoniodev@gmail.com>
2022-09-02 09:08:16 +0200

    Horários funcionando!

--- ex12/js/script.js
+++ ex12/js/script.js
@@ -8,7 +8,14 @@ function caregar(){
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if(hora >= 0 && hora < 12){
+        imagem.src = 'img/foto-morning.jpg'
         msg.innerHTML = `Bom Dia! <br> Agora são ${hora}h:${minutos}m.`
+    }else if(hora >= 12 && hora < 18){
+        imagem.src = 'img/foto-tarde.jpg'
+        msg.innerHTML = `Boa Trade! <br> Agora são ${hora}h:${minutos}m.`
+    }else{
+        imagem.src = 'img/foto-noite.jpg'
+        msg.innerHTML = `Boa Noite! <br> Agora são ${hora}h:${minutos}`
     }
 
 }
