c61dae181b24f0018d9a0271bc14cc6183ae60a1
ericiantonio <ericantoniodev@gmail.com>
2022-09-02 23:01:14 +0200

    mexendo com os homens

--- verificador de iades/js/script.js
+++ verificador de iades/js/script.js
@@ -8,9 +8,38 @@ function verificar(){
     if(fano.value.length == 0 || Number(fano.value) > ano){
         window.alert('Verifique o ano Novamente!')
     }else{
-       var radsex = window.document.getElementsByName('radsex')
+       var fsex = window.document.getElementsByName('radsex')
        var idade = ano - Number(fano.value)
-       res.innerHTML = `Idade calculada: ${idade}`
+       var genero = ''
+       var img = document.createElement('img')
+       img.setAttribute('id', 'foto')
+
+       if(fsex[0].checked){
+            genero = 'Homem'
+            if(idade >=0 && idade <= 10){
+                img.setAttribute('src','img/criancas/foto-menino.jpg')
+            }else if(idade < 21){
+                img.setAttribute('src','img/jovens/foto-rapaz.jpg')
+            }else if(idade < 50){
+                img.setAttribute('src','img/adulto/foto-adulto.jpg')
+            }else{
+                img.setAttribute('src','img/idosos/foto-idoso.jpg')
+            }
+       }else if(fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >=0 && idade <= 10){
+                //crian'ca
+            }else if(idade < 21){
+                //jovem
+            }else if(idade < 50){
+                //adulto
+            }else{
+                //idoso
+            }
+       }
+       res.style.textAlign = 'center'
+       res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+       res.appendChild(img)
     }
 
 }
