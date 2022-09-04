d1ad0fbb857ff4e037396070ec6e9f43a8c5fe6a
ericiantonio <ericantoniodev@gmail.com>
2022-09-04 17:51:59 +0200

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,4 +1,8 @@
 let num = [14,90,33,54,25]
 num.push(1)
 num.sort()
-console.log(`O nosso vector e ${num} e tem ${num.length}`)
+//console.log(`O nosso vector e ${num} e tem ${num.length}`)
+console.log(num)
+for(var i=0; i < num.length; i++){
+    console.log(`Na posição ${i} temos ${num[i]}`)
+}
