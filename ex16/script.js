80c4fd4350455c5653899b5576f8830c2e9828cb
ericiantonio <ericantoniodev@gmail.com>
2022-09-04 19:56:11 +0200

    P

--- ex16/script.js
+++ ex16/script.js
@@ -1,4 +1,16 @@
-function adicionar(){
+/*function adicionar(){
     let lista = []
+    var  res = window.document.getElementById('st')
     
-}
+    var num =  window.document.getElementById('num')
+    if(num.value.length == 0){
+        window.alert('escreve crl!')
+    }else{
+        valor = Number(num.value)
+        lista.push(valor)
+        let item = document.createElement('option')
+        item.text=`Numero ${value} adicionado!`
+        item.value =`res`
+        res.appendChild(item)
+    }
+}*/
